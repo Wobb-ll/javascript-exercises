a489334f60c1a76ba882d4be9142d2744ee31f93
Wobb-ll <222327660+Wobb-ll@users.noreply.github.com>
2025-09-01 11:24:10 -0400

    Complete exercise 05_joinStrings

--- 05_joinStrings/joinStrings.js
+++ 05_joinStrings/joinStrings.js
@@ -9,9 +9,17 @@
 
 // Add your code right below, good luck!
 
+const firstName = "Carlos";
+const lastName = "Stevenson";
+const thisYear = 1965;
+const birthYear = 1947;
 
+let greeting = `Hello! My name is ${firstName} ${lastName} and I am ${thisYear - birthYear} years old.`;
 
+let fullName = firstName + " " + lastName;
+let age = thisYear - birthYear;
 
+console.log(greeting);
 
 // Do not change this
 module.exports = {
