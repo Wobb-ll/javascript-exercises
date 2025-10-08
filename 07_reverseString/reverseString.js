c598f36f4c73ae299e29f5dddaa4dda5a8d58585
Wobb-ll <222327660+Wobb-ll@users.noreply.github.com>
2025-10-08 12:05:46 -0400

    Complete 07_reverseString
    It is ugly, however, I found methods needed
    and implemented them without help.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const arrString = string.split('');
+    arrString.reverse();
+    const output = arrString.join('');
+    return output;
 };
 
 // Do not edit below this line
