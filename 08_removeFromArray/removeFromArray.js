cabb3970b28abfe2cac1871c7e48808a674dd9cd
Wobb-ll <222327660+Wobb-ll@users.noreply.github.com>
2025-10-08 14:03:07 -0400

    Started exercise 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    for(let i = 0; i < remove.length; i++) {
+        if (array.includes(remove[i])) {
+            array.splice(array.indexOf(remove[i]), 1); 
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
