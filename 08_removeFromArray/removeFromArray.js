01a65b25348fb4a78cad403483384439abd8dec9
Wobb-ll <222327660+Wobb-ll@users.noreply.github.com>
2025-10-10 14:57:55 -0400

    Complete exercise 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,10 +1,15 @@
-const removeFromArray = function(array, ...remove) {
-    for(let i = 0; i < remove.length; i++) {
-        if (array.includes(remove[i])) {
-            array.splice(array.indexOf(remove[i]), 1); 
-        }
-    }
-    return array;
+// ---BEFORE LOOKING AT SOLUTION---
+// const removeFromArray = function(array, ...remove) {
+//     for(let i = 0; i < remove.length; i++) {
+//         if (array.includes(remove[i])) {
+//             array.splice(array.indexOf(remove[i]), 1); 
+//         }
+//     }
+//     return array;
+// };
+
+const removeFromArray = function(arr, ...remove) {
+    return arr.filter(val => !remove.includes(val))
 };
 
 // Do not edit below this line
