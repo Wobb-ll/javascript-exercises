43af949486339e9bb775b065daba23c1ee2df21a
Wobb-ll <222327660+Wobb-ll@users.noreply.github.com>
2025-10-10 15:51:52 -0400

    Complete exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0) {
+        return "ERROR"
+    }
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR"
+    }
+    const sum = [];
+    if(a < b) {
+        for (i = 0; a <= b; i++) {
+        sum[i] = a;
+        a++
+    }} else if (a > b) {
+        for (i = 0; b <= a; i++) {
+            sum[i] = b;
+            b++
+        } 
+    }
+    const finalSum = sum.reduce((total, val) => total + val);
+    return finalSum;
 };
 
 // Do not edit below this line
